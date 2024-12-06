64be1b321b4326ef9dc358021310f1c632523dc3
abuba0a <168736244+abuba0a@users.noreply.github.com>
2024-12-06 07:21:31 +0300

    Update models.py

--- M_19/task1/models.py
+++ M_19/task1/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Buyer(models.Model):
     name = models.CharField(max_length=50)
     balance = models.DecimalField(max_digits=10, decimal_places=3)
-    age = models.IntegerField()
+    age = models.PositiveIntegerField()
 
     def __str__(self):
         return self.name
